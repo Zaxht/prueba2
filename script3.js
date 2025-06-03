5b4d87e8c409b5ca1833a23290ab0e3441b3c4a6
Zaxht <fernandodistorsions@gmail.com>
2025-06-03 11:17:13 -0400

    df

---
+++ script3.js
@@ -0,0 +1,45 @@
+//telefono debe tener exactamente 8 digitos, no es obligatoria
+//contrasenia debe tener mas de 5 caracteres, es obligatoria
+
+
+//telefono debe tener exactamente 8 digitos, no es obligatoria
+//contrasenia debe tener mas de 5 caracteres, es obligatoria
+
+
+
+function validar(){
+    let button = document.getElementById("button")
+    let telefonoInput = document.getElementById(telefono)
+    let contrasenia = document.getElementById("password")
+    console.log(telefonoInput)
+
+    if telefonoInput == 8{
+        let telefonoInpunt = valid
+    }else{
+        alert("El telefono debe tener exactamente 8 digitos")
+
+    }
+
+
+    if contrasenia < 5{
+        alert("la contreseña debe tener mas de 5 carcteres")
+
+    }else{
+        let contrasenia = True
+    }
+}
+
+
+
+function almacenarInformacion(){
+
+
+}
+
+
+function mostrarDatosTabla(){}
+
+function eliminarDatos(){}
+
+
+function actualizarDatos(){}
